b37d34f698ff5c5e012e87b7edd8fd1d81566944
ws <kingwsi@icloud.com>
2022-11-16 17:49:51 +0800

    Add QR code

--- src/main/java/com/example/lanstream/StreamController.java
+++ src/main/java/com/example/lanstream/StreamController.java
@@ -1,18 +1,30 @@
 package com.example.lanstream;
 
 import com.example.lanstream.pojo.Message;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -20,6 +32,9 @@ public class StreamController {
 
 	@Autowired
 	private ArrayBlockingQueue<Message> arrayBlockingQueue;
+	
+	@Value("${host.url}")
+	private String hostUrl;
 
 	@MessageMapping("/send")
 	@SendTo("/topic/message")
@@ -37,5 +52,17 @@ public class StreamController {
 	@ResponseBody
 	public List<Message> greeting() {
 		return new ArrayList<>(arrayBlockingQueue);
+	}
+	
+	@GetMapping("/qr")
+	public ResponseEntity<byte[]> getQr(@RequestParam(name = "width", defaultValue = "100") int width, @RequestParam(value = "height", defaultValue = "100") int height) throws IOException, WriterException {
+		QRCodeWriter qrCodeWriter = new QRCodeWriter();
+		BitMatrix bitMatrix = qrCodeWriter.encode(hostUrl, BarcodeFormat.QR_CODE, width, height);
+		ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+		MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+		byte[] qrCode = pngOutputStream.toByteArray();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_PNG);
+		return new ResponseEntity<>(qrCode, headers, HttpStatus.CREATED);
 	}
 }

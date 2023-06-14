659e90a2f16df7974a3c9d50d809ba010752ae7f
ws <shu.wang@greencloud.com.cn>
2023-06-14 11:02:09 +0800

    navigator.clipboard

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -190,7 +190,7 @@ function downloadFile(url) {
 function copyCode(button) {
     var code = button.previousElementSibling.textContent;
 
-    navigator.clipboard.writeText(code)
+    copyToClipboard(code)
         .then(function() {
             // 复制成功后的处理逻辑
             button.textContent = '已复制';
@@ -202,6 +202,31 @@ function copyCode(button) {
             // 复制失败后的处理逻辑
             console.error('复制失败:', error);
         });
+}
+
+function copyToClipboard(textToCopy) {
+    // navigator clipboard 需要https等安全上下文
+    if (navigator.clipboard && window.isSecureContext && false) {
+        // navigator clipboard 向剪贴板写文本
+        return navigator.clipboard.writeText(textToCopy);
+    } else {
+        // 创建text area
+        let textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+        // 使text area不在viewport，同时设置不可见
+        textArea.style.position = "absolute";
+        textArea.style.opacity = 0;
+        textArea.style.left = "-999999px";
+        textArea.style.top = "-999999px";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        return new Promise((res, rej) => {
+            // 执行复制命令并移除文本框
+            document.execCommand('copy') ? res() : rej();
+            textArea.remove();
+        });
+    }
 }
 
 function loadUrlPath(){
